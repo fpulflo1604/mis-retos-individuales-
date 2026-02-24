2b94d339e929ee2765373dbbd8cc75b34f255939
Francisco de Paula Pulido Flores <fpulflo1604@g.educaand.es>
2026-02-24 09:44:10 +0100

    tareas 2.2.2 y 2.2.3

---
+++ script_2_2_2.js
@@ -0,0 +1,8 @@
+// 1. Seleccionamos el título
+const titulo = document.querySelector("h1");
+
+// 2. Cambiamos su texto
+titulo.textContent = "¡DOM controlado por Francisco de Paula ";
+
+// 3. Cambiamos su color de fondo
+titulo.style.backgroundColor = "sky blue"; 
